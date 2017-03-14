3862683be3e45f915d0580760af837ce0cc7bc44
Gabi Procell <gprocell927@gmail.com>
2017-03-14 12:43:32 -0600

    completes tutorial

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const md5 = require('md5')
+const fs = require('fs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = "Secret Box"
@@ -10,6 +17,15 @@ app.locals.secrets = [{
 
 app.get('/', (req, res) => {
   res.send('It\'s a secret to everyone.')
+})
+
+
+app.post('/api/v1/secrets', (request, response) => {
+  const { message } = request.body
+  const id = md5(message)
+
+  app.locals.secrets.push({ id, message })
+  response.json({ id, message })
 })
 
 app.get('/api/v1/secrets/:id', (req, res) => {
@@ -25,6 +41,39 @@ app.get('/api/v1/secrets/:id', (req, res) => {
   res.json(secret)
 })
 
+app.get('/api/v1/secrets', (request, response) => {
+  response.json(app.locals.secrets)
+})
+
+app.put('/api/v1/secrets/:id', (request, response) => {
+  const { message } = request.body
+  const { id } = request.params
+  let secret = app.locals.secrets.find(secret => {
+    return secret.id == id
+  })
+
+  secret.message = message
+
+  if(!secret) { return response.sendStatus(404) }
+  response.json(secret)
+})
+
+app.delete('/api/v1/secrets/:id', (request, response) => {
+  const { id } = request.params
+
+  app.locals.secrets = app.locals.secrets.filter((secret) => {
+    return secret.id !== id
+  })
+
+  if(!secret) { return response.sendStatus(404) }
+  response.json(app.locals.secrets)
+})
+
+app.get('/', (request, response) => {
+  fs.readFile(`${__dirname}/index.html`, (err, file) => {
+    response.send(file)
+  })
+})
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
 })
